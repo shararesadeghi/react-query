da934e91c73ec195fd3b1d3df2644c92639f384d
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-01-03 00:37:53 +0330

    useQuery for fetch data with axios

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from 'axios';
 
 
 const HomePage = () => {
 
-    const fetchUsers = async ()=>{
-        const res = await fetch("https://jsonplaceholder.typicode.com/users");
-        return res.json();
-    }
+    // const fetchUsers = async ()=>{
+    //     const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    //     return res.json();
+    // }
+    const fetchUsers = () =>
+      axios.get("https://jsonplaceholder.typicode.com/users");
 
     const {data,isLoading} = useQuery({queryKey:["users"],queryFn:fetchUsers})
    if(isLoading) return <h1>Loading...</h1>
-  return (
-    <div>{data.map(item=><h3 key={item.id}>{item.name}</h3>)}</div>
-  )
+//   return (
+//     <div>{data.map(item=><h3 key={item.id}>{item.name}</h3>)}</div>
+//   )
+return(<div>{data.data.map(item=><h3 key={item.id}>{item.name}</h3>)}</div>)
 }
 
 export default HomePage
