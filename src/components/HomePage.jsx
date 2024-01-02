433211f7f3f70ddb9d55d4cc7ff731bd3ed57fe2
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2024-01-03 00:32:39 +0330

    useQuery hook from tanstck

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
 
 
 const HomePage = () => {
+
+    const fetchUsers = async ()=>{
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        return res.json();
+    }
+
+    const {data,isLoading} = useQuery({queryKey:["users"],queryFn:fetchUsers})
+   if(isLoading) return <h1>Loading...</h1>
   return (
-    <div>HomePage</div>
+    <div>{data.map(item=><h3 key={item.id}>{item.name}</h3>)}</div>
   )
 }
 
